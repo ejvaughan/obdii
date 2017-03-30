1f188a1d4edc04f1a21db1e551ba79210abe56b6
Ethan Vaughan <ejvaughan@gmail.com>
2017-03-30 15:24:39 -0500

    Add demo 2 files

---
+++ demo2_web/aws-configuration.js
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2015-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+/*
+ * NOTE: You must set the following string constants prior to running this
+ * example application.
+ */
+var awsConfiguration = {
+   poolId: 'us-east-1:ee869f3c-fb26-4496-838b-79c4460286fd', // 'YourCognitoIdentityPoolId'
+   region: 'us-east-1' // 'YourAwsRegion', e.g. 'us-east-1'
+};
+module.exports = awsConfiguration;
