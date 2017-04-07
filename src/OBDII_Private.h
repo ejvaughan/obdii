4d10a488c5e85d7d8f7595ffcf160cdefc5df48b
Ethan Vaughan <ejvaughan@gmail.com>
2017-04-07 13:45:38 -0500

    Remove unused OBDII_Private.h header

--- src/OBDII_Private.h
+++
@@ -1,7 +0,0 @@
-#ifndef __OBDII_PRIVATE_H
-#define __OBDII_PRIVATE_H
-
-#define GET_COMMAND_MODE(command) (command)->payload[0]
-#define GET_COMMAND_PID(command) (command)->payload[1]
-
-#endif /* OBDII_Private.h */
