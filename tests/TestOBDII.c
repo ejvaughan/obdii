895c61ccc42a77cd6ecdb08a02f30bac1feeed43
Ethan Vaughan <ejvaughan@gmail.com>
2017-05-05 10:53:42 -0500

    Improve test coverage

--- tests/TestOBDII.c
+++ tests/TestOBDII.c
@@ -2,6 +2,27 @@
 #include "unity.h"
 #include "unity_fixture.h"
 
+#define SUCCESSFUL_RESPONSE_PREFIX(command) (command)->payload[0] + 0x40, (command)->payload[1]
+
+static void TestBitfield(OBDIICommand *command) {
+	unsigned int bitfieldLength = command->expectedResponseLength - 2;
+	unsigned char exampleResponsePayload[] = { SUCCESSFUL_RESPONSE_PREFIX(command), 0xA, 0xB, 0xC, 0xD };
+
+	OBDIIResponse response = OBDIIDecodeResponseForCommand(command, exampleResponsePayload, command->expectedResponseLength);
+
+	TEST_ASSERT(response.success);
+
+	if (bitfieldLength == 1) {
+		TEST_ASSERT_EQUAL_HEX8(0x0A, response.bitfieldValue);
+	} else if (bitfieldLength == 2) {
+		TEST_ASSERT_EQUAL_HEX16(0x0A0B, response.bitfieldValue);
+	} else if (bitfieldLength == 4) {
+		TEST_ASSERT_EQUAL_HEX32(0x0A0B0C0D, response.bitfieldValue);
+	}
+
+	OBDIIResponseFree(&response);
+}
+
 TEST_GROUP(OBDII);
 
 TEST_SETUP(OBDII)
@@ -12,14 +33,14 @@ TEST_TEAR_DOWN(OBDII)
 {
 }
 
-#define SUCCESSFUL_RESPONSE_PREFIX(command) (command)->payload[0] + 0x40, (command)->payload[1]
-
 TEST(OBDII, DecodeDTCs)
 {
-	// Encodes trouble codes P38AB, U20FC, and C17DE (no idea if real trouble codes)
-	unsigned char exampleResponsePayload[] = {  SUCCESSFUL_RESPONSE_PREFIX(OBDIICommands.DTCs), 0x38, 0xAB, 0xE0, 0xFC, 0x57, 0xDE };
+	OBDIICommand *command = OBDIICommands.DTCs;
 
-	OBDIIResponse response = OBDIIDecodeResponseForCommand(OBDIICommands.DTCs, exampleResponsePayload, sizeof(exampleResponsePayload));
+	// Encodes trouble codes P38AB, U20FC, and C17DE (no idea if real trouble codes)
+	unsigned char exampleResponsePayload[] = {  SUCCESSFUL_RESPONSE_PREFIX(command), 0x38, 0xAB, 0xE0, 0xFC, 0x57, 0xDE };
+
+	OBDIIResponse response = OBDIIDecodeResponseForCommand(command, exampleResponsePayload, sizeof(exampleResponsePayload));
 
 	TEST_ASSERT(response.success);
 
@@ -27,5 +48,66 @@ TEST(OBDII, DecodeDTCs)
 	TEST_ASSERT_EQUAL_STRING("P38AB", response.DTCs.troubleCodes[0]);
 	TEST_ASSERT_EQUAL_STRING("U20FC", response.DTCs.troubleCodes[1]);
 	TEST_ASSERT_EQUAL_STRING("C17DE", response.DTCs.troubleCodes[2]);
+
+	OBDIIResponseFree(&response);
 }
 
+TEST(OBDII, mode1SupportedPIDs_1_to_20)
+{
+	TestBitfield(OBDIICommands.mode1SupportedPIDs_1_to_20);
+}
+
+TEST(OBDII, monitorStatus)
+{
+	TestBitfield(OBDIICommands.monitorStatus);
+}
+
+TEST(OBDII, fuelSystemStatus)
+{
+	TestBitfield(OBDIICommands.fuelSystemStatus);
+}
+
+TEST(OBDII, commandedSecondaryAirStatus)
+{
+	TestBitfield(OBDIICommands.commandedSecondaryAirStatus);
+}
+
+TEST(OBDII, oxygenSensorsPresentIn2Banks)
+{
+	TestBitfield(OBDIICommands.oxygenSensorsPresentIn2Banks);
+}
+
+TEST(OBDII, conformingStandards)
+{
+	TestBitfield(OBDIICommands.conformingStandards);
+}
+
+TEST(OBDII, oxygenSensorsPresentIn4Banks)
+{
+	TestBitfield(OBDIICommands.oxygenSensorsPresentIn4Banks);
+}
+
+TEST(OBDII, auxiliaryInputStatus)
+{
+	TestBitfield(OBDIICommands.auxiliaryInputStatus);
+}
+
+TEST(OBDII, mode1SupportedPIDs_21_to_40)
+{
+	TestBitfield(OBDIICommands.mode1SupportedPIDs_21_to_40);
+}
+
+TEST(OBDII, mode1SupportedPIDs_41_to_60)
+{
+	TestBitfield(OBDIICommands.mode1SupportedPIDs_41_to_60);
+}
+
+TEST(OBDII, currentDriveCycleMonitorStatus)
+{
+	TestBitfield(OBDIICommands.currentDriveCycleMonitorStatus);
+}
+
+TEST(OBDII, mode9SupportedPIDs)
+{
+	TestBitfield(OBDIICommands.mode9SupportedPIDs);
+}
--- tests/test_runners/TestOBDII_Runner.c
+++ tests/test_runners/TestOBDII_Runner.c
@@ -4,4 +4,16 @@
 TEST_GROUP_RUNNER(OBDII)
 {
 	RUN_TEST_CASE(OBDII, DecodeDTCs);
+	RUN_TEST_CASE(OBDII, mode1SupportedPIDs_1_to_20);
+	RUN_TEST_CASE(OBDII, monitorStatus);
+	RUN_TEST_CASE(OBDII, fuelSystemStatus);
+	RUN_TEST_CASE(OBDII, commandedSecondaryAirStatus);
+	RUN_TEST_CASE(OBDII, oxygenSensorsPresentIn2Banks);
+	RUN_TEST_CASE(OBDII, conformingStandards);
+	RUN_TEST_CASE(OBDII, oxygenSensorsPresentIn4Banks);
+	RUN_TEST_CASE(OBDII, auxiliaryInputStatus);
+	RUN_TEST_CASE(OBDII, mode1SupportedPIDs_21_to_40);
+	RUN_TEST_CASE(OBDII, mode1SupportedPIDs_41_to_60);
+	RUN_TEST_CASE(OBDII, currentDriveCycleMonitorStatus);
+	RUN_TEST_CASE(OBDII, mode9SupportedPIDs);
 }
