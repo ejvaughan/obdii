9261869f633bbedc862ff7a9944cbda3be7e430a
Håvard F. Aasen <havard.f.aasen@pfft.no>
2022-12-29 13:50:54 +0100

    Make library link with both C and C++
    Wrap functions and variables in public headers with "extern C" so as to
    make the library link with both C and C++.

--- src/OBDIIDaemon.h
+++ src/OBDIIDaemon.h
@@ -1,6 +1,10 @@
 #ifndef __OBDII_DAEMON_H
 #define __OBDII_DAEMON_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef enum {
 	OBDIIDaemonRequestOpenSocket,
 	OBDIIDaemonRequestCloseSocket
@@ -18,5 +22,9 @@ typedef enum {
 #define OBDII_DAEMON_REQUEST_HEADER_SIZE 4
 
 #define OBDII_DAEMON_SOCKET_PATH "/tmp/obdiid.sock"
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* OBDIIDaemon.h */
